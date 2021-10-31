886f2c771c0ddca124f076e60187a098af62df31
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-10-31 21:45:32 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/jc_scrollablecolumn/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_scrollablecolumn/MainActivity.kt
@@ -3,11 +3,22 @@ package com.rujirakongsomran.jc_scrollablecolumn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_scrollablecolumn.ui.theme.JC_ScrollableColumnTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +28,32 @@ class MainActivity : ComponentActivity() {
             JC_ScrollableColumnTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    ScrollableColumn()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ScrollableColumn() {
+    val items by remember {
+        mutableStateOf((1..20).map { "Item $it" })
+    }
+
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier.verticalScroll(state = scrollState)
+    ) {
+        for(item in items){
+            Text(modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 24.dp),
+                text = item,
+                fontSize = MaterialTheme.typography.h5.fontSize,
+                fontWeight = FontWeight.Medium
+            )
         }
     }
 }
@@ -33,6 +67,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JC_ScrollableColumnTheme {
-        Greeting("Android")
+        ScrollableColumn()
     }
 }
